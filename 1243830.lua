7fa3765f71374b83efe27f5fe3ce315fe464dc0b
ehgen0ng <26219737+ehgen0ng@users.noreply.github.com>
2025-07-27 11:31:15 +0800

    Add files via upload

---
+++ 1243830.lua
@@ -0,0 +1,25 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games
+-- Steam App 1243830 Manifest
+-- Name: Overcooked! All You Can Eat
+-- Generated: 2025-06-06 16:54:56
+-- Total Depots: 4
+-- Total DLCs: 1
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(1243830) -- Overcooked! All You Can Eat
+
+-- MAIN APP DEPOTS
+addappid(1243831, 1, "ee4fef3dceab1e96eb608bde52106366ade46c83bf1db1432b02b5f5e3d9c858") -- Project Bagel Content
+setManifestid(1243831, "3729927846043242105", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- .NET 4.8 Redist (Shared from App 228980)
+setManifestid(229007, "4477590687906973371", 0)
+
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1528250) -- Overcooked All You Can Eat - Swedish Chef
