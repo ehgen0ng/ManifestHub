43d87dba123c2742e31b1dfffcd97a8a162c1841
ehgen0ng <26219737+ehgen0ng@users.noreply.github.com>
2025-07-27 11:29:33 +0800

    Add files via upload

---
+++ 2993780.lua
@@ -0,0 +1,26 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games!
+-- Steam App 2993780 Manifest
+-- Name: FANTASY LIFE i: The Girl Who Steals Time
+-- Generated: 2025-07-24 10:53:03
+-- Total Depots: 4
+-- Total DLCs: 1
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(2993780, 1, "722d0247a3577ac776db1307727b1de8b95970c2fae08886f22de9fd9d56d202") -- FANTASY LIFE i: The Girl Who Steals Time
+
+-- MAIN APP DEPOTS
+addappid(2993781, 1, "ad98d2baa7c277a9a3d59599e17cb30eb75781386d8c475317d4570b8dc72aaf") -- Depot 2993781
+setManifestid(2993781, "5364509046839958588", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- .NET 4.8 Redist (Shared from App 228980)
+setManifestid(229007, "4477590687906973371", 0)
+
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3464380) -- FANTASY LIFE i The Girl Who Steals Time  Edition Upgrade (Digital Deluxe)
+addtoken(3464380, "13290660664749164908")
