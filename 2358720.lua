6be6e4e2d0264332ed19cb76878a95714b2acc94
ehgen0ng <26219737+ehgen0ng@users.noreply.github.com>
2025-07-28 13:22:12 +0800

    Add files via upload

---
+++ 2358720.lua
@@ -0,0 +1,35 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games
+-- Steam App 2358720 Manifest
+-- Name: Black Myth: Wukong
+-- Generated: 2025-06-05 00:38:24
+-- Total Depots: 7
+-- Total DLCs: 2
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(2358720) -- Black Myth: Wukong
+
+-- MAIN APP DEPOTS
+addappid(2358721, 1, "78d5a284965b2206de191dc4ca99d43c9caf5b5e78b47cf0c366fbbb884190d8") -- Main Game Content (Windows Content)
+setManifestid(2358721, "2985710911012900154", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "550968249685141759", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") -- .NET 4.7 Redist (Shared from App 228980)
+setManifestid(229006, "1784011429307107530", 0)
+
+-- DLCS WITH DEDICATED DEPOTS
+-- Black Myth Wukong Deluxe Edition Upgrade (AppID: 2672610)
+addappid(2672610)
+addappid(2672611, 1, "9f7513ce3953f71d2f994a9a331b862c8a41aeae89b0ff0933336aeef1796220") -- Black Myth Wukong Deluxe Edition Upgrade - Main Content
+setManifestid(2672611, "1995373587248188296", 0)
+
+-- Black Myth Wukong Soundtrack Selection (AppID: 3288260)
+addappid(3288260)
+addappid(3288261, 1, "dd58656a765e3490f9479f111d16728b1ffa1b0b474076884b122c344153ff52") -- Black Myth Wukong Soundtrack Selection - Main Content
+setManifestid(3288261, "1420688854810981808", 0)
+addappid(3288262, 1, "d00c5648814e85abd2a3d949042a2a715d5a3b9b437c754479b01ef7fd74e42a") -- Black Myth Wukong Soundtrack Selection - Main Content
+setManifestid(3288262, "7921052030878882502", 0)
