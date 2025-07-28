62e819b016f92c87efdc48a01ab6047329451a02
ehgen0ng <26219737+ehgen0ng@users.noreply.github.com>
2025-07-28 17:16:18 +0800

    Add files via upload

---
+++ 2531310.lua
@@ -0,0 +1,25 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games!
+-- Steam App 2531310 Manifest
+-- Name: The Last of Us™ Part II Remastered
+-- Generated: 2025-07-22 18:11:48
+-- Total Depots: 4
+-- Total DLCs: 1
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2531310) -- The Last of Us™ Part II Remastered
+
+-- MAIN APP DEPOTS
+addappid(2531311, 1, "f7506952e736e71547c2d3c2c82959438e836c66d5b8b8f3208ccc082f60df23") -- Depot 2531311
+setManifestid(2531311, "4566212634007989287", 0)
+addappid(2531313, 1, "a88758211f172f53efb57ad5c819b9d5f34b0796b44bd876639a09029e6a8089") -- Depot 2531313
+setManifestid(2531313, "4994458262959665537", 0)
+addappid(2531314, 1, "3dc1a7d997476f1b4eb3a78f11af1be0d750a14719dc5d5eaeb940bcd44b74e5") -- Depot 2531314
+setManifestid(2531314, "6262322802969244361", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 0)
+
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3420050) -- The Last of Us Part II Remastered - Pre-purchase Entitlements
